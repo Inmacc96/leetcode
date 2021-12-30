a83abda74a9e527c77eeec52c2a0f29043f5d4c1
inmacc96 <inmacaballerocarrero@gmail.com>
2021-12-30 18:09:30 +0100

    Fixing add digits problem in python.

--- easy/add-digits/add-digits.py
+++ easy/add-digits/add-digits.py
@@ -1,10 +1,6 @@
-def addDigits(self, num):
-        """
-        :type num: int
-        :rtype: int
-        """
-        listdig = [int(x) for x in str(num)]
-        while len(listdig)!=1:
-            suma = sum(listdig)
-            listdig= [int(x) for x in str(suma)]
-        return listdig[0]
+def addDigits(num):
+    listdig = [int(x) for x in str(num)]
+    while len(listdig)!=1:
+        suma = sum(listdig)
+        listdig= [int(x) for x in str(suma)]
+    return listdig[0]
