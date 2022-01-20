173e153a182b2e1edcb858447c92cce8cfb44c69
inmacc96 <inmacaballerocarrero@gmail.com>
2022-01-20 18:04:45 +0100

    Removing unnecessary lines of code in count-words.js

--- easy/count_commonords_with_one_occurrence/count-words.js
+++ easy/count_commonords_with_one_occurrence/count-words.js
@@ -1,66 +1,32 @@
-var countWords = function(words1, words2) {
-    let n_words = 0;
-    let words1_occ = {};
-    let words2_occ = {};
-    for(let i=0; i < words1.length; i++){
-        if (words1[i] in words1_occ){
-            words1_occ[words1[i]] += 1
-        }
-        else{
-            words1_occ[words1[i]] = 1
-        }
+var countWords = function (words1, words2) {
+  let n_words = 0;
+  let words1_occ = {};
+  let words2_occ = {};
+  for (let i = 0; i < words1.length; i++) {
+    if (words1[i] in words1_occ) {
+      words1_occ[words1[i]] += 1;
+    } else {
+      words1_occ[words1[i]] = 1;
     }
+  }
 
-    for(let i=0; i < words2.length; i++){
-        if (words2[i] in words2_occ){
-            words2_occ[words2[i]] += 1
-        }
-        else{
-            words2_occ[words2[i]] = 1
-        }
+  for (let i = 0; i < words2.length; i++) {
+    if (words2[i] in words2_occ) {
+      words2_occ[words2[i]] += 1;
+    } else {
+      words2_occ[words2[i]] = 1;
     }
+  }
 
-    for(word in words1_occ){
-        if(word in words2_occ & words1_occ[word] == 1 & words2_occ[word] == 1){
-            n_words +=1;
-        }
+  for (word in words1_occ) {
+    if (
+      (word in words2_occ) &
+      (words1_occ[word] == 1) &
+      (words2_occ[word] == 1)
+    ) {
+      n_words += 1;
     }
+  }
 
-    return (n_words)
+  return n_words;
 };
-
-
-
-let words1 = ["leetcode","is","amazing","as","is"];
-let words2 = ["amazing","leetcode","is"];
-console.log(countWords(words1,words2))
-let n_words = 0 ;
-let words1_occ = {};
-let words2_occ = {};
-for(let i=0; i < words1.length; i++){
-    if (words1[i] in words1_occ){
-        words1_occ[words1[i]] += 1
-    }
-    else{
-        words1_occ[words1[i]] = 1
-    }
-}
-for(let i=0; i < words2.length; i++){
-    if (words2[i] in words2_occ){
-        words2_occ[words2[i]] += 1
-    }
-    else{
-        words2_occ[words2[i]] = 1
-    }
-}
-for(word in words1_occ){
-    console.log(word)
-    if(word in words2_occ & words1_occ[word] == 1 & words2_occ[word] == 1){
-        n_words +=1;
-    }
- 
-}
-console.log(n_words)
-
-
-
