83928dc40e5dfd80fb9d8ad349c4c7d56947633d
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-19 18:02:01 +0100

    Improved sumOfThree.js code.

--- medium/sum-of-three/sumOfThree.js
+++ medium/sum-of-three/sumOfThree.js
@@ -1,9 +1,8 @@
 var sumOfThree = function (num) {
   var x = (num - 3) / 3;
   if (Number.isInteger(x)) {
-    var res = [x, x + 1, x + 2];
+    return [x, x + 1, x + 2];
   } else {
-    var res = [];
+    return [];
   }
-  return res;
 };
